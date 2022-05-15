4b36811eca7e23d577c7b8bda7dd9bce3e08bdc8
kalangithub <105605557+kalangithub@users.noreply.github.com>
2022-05-15 16:23:33 -0230

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,122 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "Kalan":return img`
+.....................
+.....fffffffff.......
+.....feeeeeeeef......
+....feeeeeeeeef......
+....feeeeeeeeeef.....
+....feeeeefdfeef.....
+....fefffffddfef.....
+....ffddddfddff......
+.....fddddfddf.......
+.....fdddddddf.......
+......ffffffff.......
+..fffff66f666f.......
+.fbbbbfffffffffffff..
+.fbbbbbf9999999fffff.
+.fbbfbbf999999fbbbbbf
+.fbbbbbf999999fbbbbbf
+.fbbbbbffffffffbbfbbf
+..fffff.......fbbbbbf
+..............fbbbbbf
+...............fffff.
+`;
+            case "image3":
+            case "Building":return img`
+ffffffffffffffff
+fbbbbfbbbbbbbbbf
+fbbbbfbbbbbbbbbf
+fbbbbfbbbbbbbbbf
+fbbbbfbbbbbbbbbf
+ffffffffffffffff
+fbbbbbbbbbfbbbbf
+fbbbbbbbbbfbbbbf
+fbbbbbbbbbfbbbbf
+fbbbbbbbbbbfbbbf
+ffffffffffffffff
+fbbfbbbbbbbbbbbf
+fbbfbbbbbbbbbbbf
+fbbfbbbbbbbbbbbf
+fbbfbbbbbbbbbbbf
+fbbfbbbbbbbbbbbf
+fbbfbbbbbbbbbbbf
+fbbbfbbbbbbbbbbf
+ffffffffffffffff
+fbbbbbbbbfbbbbbf
+fbbbbbbbbfbbbbbf
+fbbbbbbbbfbbbbbf
+fbbbbbbbbfbbbbbf
+fbbbbbbbbfbbbbbf
+ffffffffffffffff
+fbbbfbbbbbbbbbbf
+fbbbfbbbbbbbbbbf
+fbbbfbbbbbbbbbbf
+fbbbfbbbbbbbbbbf
+ffffffffffffffff
+fbbbbbbbbbfbbbbf
+fbbbbbfffffbbbbf
+fbbbbbf222fbbbbf
+fbbbbbf222fbbbbf
+fbbbbbf222fbbbbf
+fffffff222fbbbbf
+fbbbbbf222ffffff
+fbbbbbf222fbbbbf
+fbbbbbf222fbbbbf
+ffffffffffffffff
+`;
+            case "image2":
+            case "Person":return img`
+. . . . . f f f f f . . . 
+. . . f f f f f f f f f . 
+. . f f f c f f f f f f . 
+. . f f c f f f c f f f f 
+f f c c f f f c c f f c f 
+f f f f f e f f f f c c f 
+. f f f e e f f f f f f f 
+. . f f e e f b f e e f f 
+. . . f 4 4 f 1 e 4 e f . 
+. . . f 4 4 4 4 e f f f . 
+. . . f f e e e e e f . . 
+. . . f 7 7 7 e 4 4 e . . 
+. . . f 7 7 7 e 4 4 e . . 
+. . . f 6 6 6 f e e f . . 
+. . . . f f f f f f . . . 
+. . . . . . f f f . . . . 
+`;
+            case "image4":
+            case "Driving_Teacher":return img`
+. . . f f f f f . . . . . 
+. f f f f f f f f f . . . 
+. f f f f f f e f f f . . 
+f f f f e f f f e f f . . 
+f e f f e e f f f e e f f 
+f e e f f f f d f f f f f 
+f f f f f f f d d f f f . 
+f f d d f 1 f d d f f . . 
+. f d d f d d d f f . . . 
+. f f f f d f f d f . . . 
+. . f f f f f f f f . . . 
+. . f d d f 4 4 4 f . . . 
+. . f d d f 4 4 4 f . . . 
+. . f f f f 2 2 2 f . . . 
+. . . f f f f f f . . . . 
+. . . . f f f . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
